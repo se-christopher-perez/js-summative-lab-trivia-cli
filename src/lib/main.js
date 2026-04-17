ab6f8415c9d831068810a08d69d906e689067419
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-04-17 13:40:59 -0400

    Starter code for cli

---
+++ src/lib/main.js
@@ -0,0 +1,130 @@
+
+import { select, input, Separator } from '@inquirer/prompts';
+import { triviaQuestions } from './triviaQuestions.js';
+
+const data = triviaQuestions
+
+export async function mainMenu() {
+
+    const selectAction = await select({
+        message: "Main Menu",
+        choices: [
+            { name: "Start Game", value: "start" },
+            { name: "See stat", value: "stat" },
+            { name: "See Questions", value: "questions" },
+            { name: "Settings", value: "settings" },
+            { name: "Quit", value: "quit" }
+        ],
+        loop: false
+    })
+
+    if (selectAction === "start") {
+        console.log(selectAction)
+    } else if (selectAction === "stats") {
+        console.log(selectAction)
+    } else if (selectAction === "questions") {
+        questionMenu()
+    } else if (selectAction === "settings") {
+        settingsMenu()
+    } else if (selectAction === "quit") {
+        console.log(selectAction)
+    }
+
+}
+
+///////////QUESTIONS///////////
+
+export async function questionMenu() {
+
+    let dataMap = data.map((element) => {
+        return { name: element["question"], value: element["answer"] }
+    })
+
+    const selectAction = await select({
+        message: 'Select question for answer; "Back" to return to Main Menu',
+        choices: [
+            ...dataMap,
+            { name: "Back", value: "back" }
+        ],
+        loop: false
+    });
+
+    if (selectAction === "back") {
+        await mainMenu()
+    } else {
+        console.log(selectAction)
+        await displayQuestions()
+    };
+
+}
+
+///////////QUESTIONS///////////
+
+///////////SETTINGS///////////
+
+export async function settingsMenu() {
+
+const selectAction = await select({
+        message: "Settings",
+        choices: [
+            { name: "Difficulty", value: "difficulty" },
+            { name: "Question Topics", value: "question topics" },
+            { name: "Back", value: "back" }
+        ],
+        loop: false
+    })
+
+    if (selectAction === "back") {
+        await mainMenu()
+    } else if (selectAction === "difficulty") {
+        await difficultyMenu()
+    } else if (selectAction === "question topics") {
+        await topicMenu()
+    }
+
+}
+
+export async function difficultyMenu() {
+
+const selectAction = await select({
+        message: "Select Topics",
+        choices: [
+            { name: "Easy", value: "easy" },
+            { name: "Medium", value: "medium" },
+            { name: "Hard", value: "hard" },
+            { name: "Back", value: "back" }
+        ],
+        loop: false
+    })
+
+    if (selectAction === "back") {
+        await settingsMenu()
+    } 
+
+}
+
+export async function topicMenu() {
+
+const selectAction = await select({
+        message: "Select Topics",
+        choices: [
+            { name: "All", value: "all" },
+            { name: "Movies/Television", value: "movies/television" },
+            { name: "Geography", value: "geography" },
+            { name: "History", value: "history" },
+            { name: "Science", value: "science" },
+            { name: "Back", value: "back" }
+        ],
+        loop: false
+    })
+
+    if (selectAction === "back") {
+        await settingsMenu()
+    } 
+
+}
+
+///////////SETTINGS///////////
+
+mainMenu()
+// displayQuestions()
---
+++ src/lib/triviaQuestions.js
@@ -0,0 +1,103 @@
+
+export const triviaQuestions = [
+    {
+        question: "Who was the first President of the United States?",
+        topic: "history",
+        answer: "George Washington"
+    },
+    {
+        question: "In which year did World War II end?",
+        topic: "history",
+        answer: "1945"
+    },
+    {
+        question: "Which ancient civilization built the Great Pyramids of Giza?",
+        topic: "history",
+        answer: "Egyptians"
+    },
+    {
+        question: "Who was known as the 'Maid of Orleans' and fought for France?",
+        topic: "history",
+        answer: "Joan of Arc"
+    },
+    {
+        question: "What was the name of the ship that transported the Pilgrims to America in 1620?",
+        topic: "history",
+        answer: "Mayflower"
+    },
+    {
+        question: "Which movie features the famous line, 'I'm going to make him an offer he can't refuse'?",
+        topic: "movies/television",
+        answer: "The Godfather"
+    },
+    {
+        question: "In the TV show 'The Office', what is the name of the paper company where the characters work?",
+        topic: "movies/television",
+        answer: "Dunder Mifflin"
+    },
+    {
+        question: "What is the name of the snowman in the movie 'Frozen'?",
+        topic: "movies/television",
+        answer: "Olaf"
+    },
+    {
+        question: "Which actor played the character of Iron Man in the Marvel Cinematic Universe?",
+        topic: "movies/television",
+        answer: "Robert Downey Jr."
+    },
+    {
+        question: "What color is Mickey Mouse's shorts?",
+        topic: "movies/television",
+        answer: "Red"
+    },
+    {
+        question: "What planet is known as the Red Planet?",
+        topic: "science",
+        answer: "Mars"
+    },
+    {
+        question: "What is the boiling point of water in degrees Celsius?",
+        topic: "science",
+        answer: "100"
+    },
+    {
+        question: "What is the name of the force that pulls objects toward the Earth?",
+        topic: "science",
+        answer: "Gravity"
+    },
+    {
+        question: "How many legs does a spider have?",
+        topic: "science",
+        answer: "8"
+    },
+    {
+        question: "Which gas do humans need to breathe in to survive?",
+        topic: "science",
+        answer: "Oxygen"
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        topic: "geography",
+        answer: "Pacific"
+    },
+    {
+        question: "Which country has the largest population in the world?",
+        topic: "geography",
+        answer: "India"
+    },
+    {
+        question: "What is the capital city of France?",
+        topic: "geography",
+        answer: "Paris"
+    },
+    {
+        question: "Which river is the longest in the world?",
+        topic: "geography",
+        answer: "Nile"
+    },
+    {
+        question: "In which country would you find the Eiffel Tower?",
+        topic: "geography",
+        answer: "France"
+    }
+];
